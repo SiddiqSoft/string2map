93ba6f6650f615001c0571328ef28b0a1c111b4b
Abdulkareem Siddiq <asiddiq@loopup.com>
2020-11-04 13:23:32 -0800

    WIP

---
+++ src/string2map.cpp
@@ -0,0 +1,16 @@
+#include <map>
+#include <unordered_map>
+
+#include "string2map.h"
+
+namespace siddiqsoftware::string2map
+{
+	template<typename ST, typename MT>
+	MT parse(ST& src) noexcept
+	{
+		MT resultMap{};
+
+
+	}
+	
+}
---
+++ src/string2map.h
@@ -0,0 +1,7 @@
+#include <string>
+
+namespace siddiqsoftware::string2map
+{
+	template<typename ST, typename MT>
+	MT parse(ST& src) noexcept;
+}
