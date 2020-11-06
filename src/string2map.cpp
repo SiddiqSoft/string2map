748f1b4b91fcbc1f7150964e5555550d0116a3c1
Abdulkareem Siddiq <asiddiq@loopup.com>
2020-11-06 01:52:13 -0800

    Updated the code to support any map type

--- src/string2map.cpp
+++ src/string2map.cpp
@@ -4,46 +4,50 @@
 #include <unordered_map>
 #include <exception>
 
-//#include "std::stringring2map.h"
+#include "string2map.h"
 
 
 namespace siddiqsoftware::string2map
 {
-	std::map<std::string, std::string>
-	parse(std::string& src, const std::string& keyDelimiter, const std::string& valueDelimiter) noexcept(false)
+	/// @brief Given a string which contains a key-value pair, extract them into a map of the same type.
+	/// @tparam T Must be either std::string or std::wstring or std::u8string
+	/// @tparam R Defaults to std::map but you can use std::multimap if you wish to tackle duplicates in the src string or std::unordered_map
+	/// @param src Must be either std::string or std::wstring or std::u8string
+	/// @param keyDelimiter Delimiter for the key portion. Example: ": " or ":" or "="
+	/// @param valueDelimiter The "line terminator" delimiter which defines the value. Example: "\r\n".
+	/// @return map of key-value elements of given type
+	template <typename T, typename R = std::map<T, T>>
+	R parse(T& src, const T& keyDelimiter, const T& valueDelimiter) noexcept(false)
 	{
-		//if constexpr (std::is_same_v<std::string, std::stringring> || std::stringd::is_same_v<std::string, std::stringd::wstd::stringring>)
+		if constexpr (std::is_same_v<T, std::string> || std::is_same_v<T, std::wstring>)
 		{
-			// Our src is std::stringd::std::stringring
-			std::map<std::string, std::string> resultMap {};
-			size_t							   keyStart = 0;
+			R resultMap {};
 
-			for (auto i = src.begin(); i != src.end();)
+			for (size_t keyStart = 0; keyStart < src.length();)
 			{
 				if (auto keyEnd = src.find(keyDelimiter, keyStart); keyEnd != std::string::npos)
 				{
 					// Found a key
-					auto key = src.substr(keyStart, keyEnd + 1 - keyStart);
+					auto key = src.substr(keyStart, keyEnd - keyStart);
 					if (auto valueEnd = src.find(valueDelimiter, keyEnd); valueEnd != std::string::npos)
 					{
-						// Found value
-						auto value = src.substr(keyEnd, valueEnd + 1 - keyEnd);
+						// Found value (make sure we skip the key delimiter length)
+						auto value = src.substr(keyEnd + keyDelimiter.length(), valueEnd - (keyEnd + keyDelimiter.length()));
 
 						// Insert element..
-						resultMap[key] = value;
+						resultMap.insert({key, value});
+						// Advance to the next potential element.
+						keyStart = valueEnd + valueDelimiter.length();
 					}
 					else
 					{
-						i++;
+						// No value end was located! We must break out of the loop
+						break;
 					}
-
-					// Advance the next starting location
-					keyStart = keyEnd;
-					i += keyStart;
 				}
 				else
 				{
-					// No key end was located; We should break out of the loop
+					// No key end was located; We must break out of the loop
 					break;
 				}
 			}
--- src/string2map.h
+++ src/string2map.h
@@ -3,5 +3,13 @@
 
 namespace siddiqsoftware::string2map
 {
-	template <typename ST> static std::map<ST, ST> parse(ST& src, const ST& keyDelimiter, const ST& valueDelimiter) noexcept(false);
-}
+	/// @brief Given a string which contains a key-value pair, extract them into a map of the same type.
+	/// @tparam T Must be either std::string or std::wstring or std::u8string
+	/// @tparam R Defaults to std::map but you can use std::multimap if you wish to tackle duplicates in the src string or std::unordered_map
+	/// @param src Must be either std::string or std::wstring or std::u8string
+	/// @param keyDelimiter Delimiter for the key portion. Example: ": " or ":" or "="
+	/// @param valueDelimiter The "line terminator" delimiter which defines the value. Example: "\r\n".
+	/// @return map of key-value elements of given type
+	template <typename T, typename R = std::map<T, T>>
+	R parse(T& src, const T& keyDelimiter, const T& valueDelimiter) noexcept(false);
+} // namespace siddiqsoftware::string2map
